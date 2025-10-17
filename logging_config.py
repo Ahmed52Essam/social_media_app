5ad0a4dfb4844038723c5b0ea4529f5e07b57bb5
ahmedessam <ahmed52essam@gmail.com>
2025-10-17 22:12:38 +0300

    adding File handlers for saving logs

--- logging_config.py
+++ logging_config.py
@@ -13,21 +13,38 @@ def configure_logging() -> None:
                     "class": "logging.Formatter",
                     "datefmt": "%Y-%m-%d %H:%M:%S",
                     "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                }
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
             },
             "handlers": {
                 "default": {
                     "class": "rich.logging.RichHandler",
                     "level": "DEBUG",
                     "formatter": "console",
-                }
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "social_media_app.log",
+                    "maxBytes": 1024 * 1024,  # 1 MB
+                    "backupCount": 5,
+                    "encoding": "utf-8",
+                },
             },
             "loggers": {
+                "uvicorn": {"handlers": ["default", "rotating_file"], "level": "INFO"},
                 "social_media_app": {
-                    "handlers": ["default"],
+                    "handlers": ["default", "rotating_file"],
                     "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
                     "propagate": False,
-                }
+                },
+                "databases": {"handlers": ["default"], "level": "WARNING"},
+                "aiosqlite": {"handlers": ["default"], "level": "WARNING"},
             },
         }
     )
