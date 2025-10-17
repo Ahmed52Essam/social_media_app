339176dd21daf8e1490ec5a673ae89cb1d3cb6f4
ahmedessam <ahmed52essam@gmail.com>
2025-10-17 22:44:08 +0300

    logging HTTPException with an exception handler

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import logging
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import http_exception_handler
 
 from social_media_app.database import database
 from social_media_app.logging_config import configure_logging
 from social_media_app.routers.post import router as post_router
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -19,6 +23,8 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(post_router)
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World!"}
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler_logging(request, exc: HTTPException):
+    logger.error(f"HTTPException: {exc.status_code} - {exc.detail}")
+    return await http_exception_handler(request, exc)
