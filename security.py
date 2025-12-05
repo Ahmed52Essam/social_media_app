a2e69921bdae709b23f8d8257af55163aa797b19
ahmedessam <ahmed52essam@gmail.com>
2025-12-05 17:11:26 +0200

    #User Email confirmation: Creating the confirmation token

--- security.py
+++ security.py
@@ -24,12 +24,26 @@ def access_token_expire_minutes() -> int:
     return 30
 
 
+def confirmation_token_expire_minutes() -> int:
+    return 1440
+
+
 def create_access_token(email: str):
     logger.debug("Creating access token", extra={"email": email})
     expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
         minutes=access_token_expire_minutes()
     )
-    jwt_data = {"sub": email, "exp": expire}
+    jwt_data = {"sub": email, "exp": expire, "type": "access"}
+    encoded_jwt = jwt.encode(jwt_data, key=SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def create_confirmation_token(email: str):
+    logger.debug("Creating confirmation token", extra={"email": email})
+    expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+        minutes=confirmation_token_expire_minutes()
+    )
+    jwt_data = {"sub": email, "exp": expire, "type": "confirmation"}
     encoded_jwt = jwt.encode(jwt_data, key=SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
@@ -65,6 +79,9 @@ async def get_current_user(token: Annotated[str, Depends(oauth2scheme)]):
         payload = jwt.decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get("sub")
         if email is None:
+            raise credintials_exception
+        type = payload.get("type")
+        if type is None or type != "access":
             raise credintials_exception
     except ExpiredSignatureError:
         raise HTTPException(
--- tests/test_security.py
+++ tests/test_security.py
@@ -10,10 +10,24 @@ async def test_access_token_expire_minutes():
 
 
 @pytest.mark.anyio
+async def test_confirmation_token_expire_minutes():
+    assert security.confirmation_token_expire_minutes() == 1440
+
+
+@pytest.mark.anyio
 async def test_create_access_token():
     token = "email@test.net"
     token = security.create_access_token(token)
-    assert {"sub": "email@test.net"}.items() <= jwt.decode(
+    assert {"sub": "email@test.net", "type": "access"}.items() <= jwt.decode(
+        token, key=security.SECRET_KEY, algorithms=[security.ALGORITHM]
+    ).items()
+
+
+@pytest.mark.anyio
+async def test_create_confirmation_token():
+    token = "email@test.net"
+    token = security.create_confirmation_token(token)
+    assert {"sub": "email@test.net", "type": "confirmation"}.items() <= jwt.decode(
         token, key=security.SECRET_KEY, algorithms=[security.ALGORITHM]
     ).items()
 
@@ -70,3 +84,10 @@ async def test_get_current_user(registered_user: dict):
 async def test_get_current_user_invalid_token():
     with pytest.raises(security.HTTPException):
         await security.get_current_user("Invalid token")
+
+
+@pytest.mark.anyio
+async def test_get_current_user_wrong_type_token(registered_user: dict):
+    token = security.create_confirmation_token(registered_user["email"])
+    with pytest.raises(security.HTTPException):
+        await security.get_current_user(token)
